533339ada3e28da1fed9e66ec37537a7a70216fa
CRISTIAN ANDRES ANDRES <>
2021-04-22 20:04:32 +0000

    Reto 3, ok

---
+++ py2_retos1/reto3.py
@@ -0,0 +1,8 @@
+n1=9
+n2=8
+
+print("La suma es: ",n1+n2)
+print("La resta es: ",n1-n2)
+print("La multiplicación es: ",n1*n2)
+print("La división es: ",n1/n2)
+print("La potencia del primer número al segundo es: ", n1**n2)
