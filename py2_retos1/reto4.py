14ba74f47039cd03d5eb3c3e9ce40aa0eab6eb77
CRISTIAN ANDRES ANDRES <>
2021-04-22 20:13:39 +0000

    Reto 4, ok

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,9 @@
+a=15
+b=14
+
+if a<b:
+  print("El primer número es menor del segundo")
+elif a>b:
+  print("El primer número es mayor del segundo")
+else :
+  print("El primer número es igual del segundo")
